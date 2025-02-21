35066c9bc006f9193017caafbad32fc028753502
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-02-21 15:04:21 +0300

    refactor: update favorite button in movieCard component to display heart icon

--- frontendMovieApp/src/components/movieCard.jsx
+++ frontendMovieApp/src/components/movieCard.jsx
@@ -1,17 +1,18 @@
+function movieCard({ movie }) {
+  function onFavoriteClick() {
+    alert("clicked");
+  }
 
-function movieCard({movie}) {
-
-    function onFavoriteClick(){
-        alert("clicked")
-    }
-
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
-            <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick}></button>
-            </div>
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            🤍
+          </button>
         </div>
-
+      </div>
     </div>
+  );
 }
