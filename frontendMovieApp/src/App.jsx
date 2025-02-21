fd44875744d872747b22195cbbcd428808b22c78
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-02-21 14:47:15 +0300

    refactor: update Text component to accept display prop instead of hardcoded message

--- frontendMovieApp/src/App.jsx
+++ frontendMovieApp/src/App.jsx
@@ -3,18 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-    <Text />
-      <Text />
+    
     </>
   );
 }
-//PROPS  stands for property
-function Text() {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  );
-}
+
 
 export default App;
